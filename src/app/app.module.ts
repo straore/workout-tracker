93e29fbf47049d645104e1a5ff3e68a3e938910b
Siaka <siaka.techbox@gmail.com>
2019-03-21 00:31:02 +0100

    add functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxLoadingModule} from 'ngx-loading';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +11,7 @@ import { EntryEditorComponent } from './entry-editor/entry-editor.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { WorkoutsApiService } from './services/workouts-api.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,12 +19,14 @@ import { HttpClientModule } from '@angular/common/http';
     HomeComponent,
     WorkoutsComponent,
     EntryEditorComponent,
-    NavMenuComponent,
-    HttpClientModule
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgxLoadingModule.forRoot({})
   ],
   providers: [WorkoutsApiService],
   bootstrap: [AppComponent]
