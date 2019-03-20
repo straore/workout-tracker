d3c7835e731cb516718a32d586660806312761da
Siaka <siaka.techbox@gmail.com>
2019-03-21 00:51:44 +0100

    configure router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EntryEditorComponent } from './entry-editor/entry-editor.component';
+import { WorkoutsComponent } from './workouts/workouts.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'workouts', component: WorkoutsComponent },
+  { path: 'workouts/:id', component: EntryEditorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
